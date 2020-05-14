995f1beef2c03b8246d7a3e85aba3f5bc065b2db
Rafael Garcia <rafagrcb@gmail.com>
2020-05-14 20:20:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,8 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array = ["Link", "Zelda","Ganon","arrays!"]
-  arrays = array.shift('wow')
+  array = ["wow", "Zelda","Ganon","arrays!"]
+  arrays = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
