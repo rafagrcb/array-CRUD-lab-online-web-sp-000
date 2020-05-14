d4004e61e4c02163bebbe3d4902b6ed239b01cd3
Rafael Garcia <rafagrcb@gmail.com>
2020-05-14 20:19:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
   array = ["Link", "Zelda","Ganon","arrays!"]
-  
+  arrays = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
